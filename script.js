c0f199cc971bbdf0c86d580847f2f27ebcb0dc43
Kyle Domingo <kyledomingo783@gmail.com>
2025-07-09 00:09:07 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+function showMessage() {
+  const message = document.getElementById("message");
+  const gift = document.querySelector(".gift");
+  const giftImage = document.getElementById('giftImage');
+  const mess = document.getElementById("mess");
+
+  // Change the gift box to the opened version
+  giftImage.src = './src/img/gift-open.png';
+  gift.classList.add('open'); 
+  
+  // Show the birthday message with a fade-in effect
+  message.style.display = "block";
+  message.style.opacity = 0; 
+
+  let opacity = 0;
+  const fadeIn = setInterval(() => {
+    if (opacity < 1) {
+      opacity += 0.05;
+      message.style.opacity = opacity;
+    } else {
+      clearInterval(fadeIn);
+    }
+  }, 30);
+}
+
+function startAudio() {
+  const audio = document.getElementById("hbdAudio");
+  audio.play().then(() => {
+    mess.remove();
+  }).catch((err) => {
+    console.log("Audio blocked:", err);
+  });
+
+  document.body.onclick = null;
+}
